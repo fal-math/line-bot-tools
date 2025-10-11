f6db054c00f9fd3b887020077327fd4818b6466b
fal-mathPC <takada.y.ak@m.titech.ac.jp>
2025-10-11 16:52:17 +0900

    Refactor LineWebhookHandler command handling

--- src/services/LineWebhookHandler.ts
+++ src/services/LineWebhookHandler.ts
@@ -17,6 +17,12 @@ export class LineWebhookHandler {
     'name'
   );
 
+  public handle(text: string, to: string) {
+    if (text === '★外部練追加★') this.replyFormat(to);
+    if (text.trim().includes('★外部練追加フォーマット★')) this.registerExternalPractice(text, to);
+  }
+
+  // 外部練追加フォーマットのキー
   private key = {
     date: '日付',
     time: '時間',
@@ -35,102 +41,100 @@ export class LineWebhookHandler {
     deadline: '〆切: 「◯/☓」の形式。例→9/13',
     category: '',
   };
-
-  public handle(text: string, to: string) {
-    if (text === '外部練追加') {
-      const categoryList = [...this.configOfExPracrice.names(), 'その他'];
-      const categoryText = `種別:\n下記のどれかを選んでください。\n${categoryList.join('/')}`;
-      this.line.pushText(
-        to,
-        [
-          '★外部練追加フォーマット★',
-          `${this.key.date}：`,
-          `${this.key.time}：`,
-          `${this.key.title}：`,
-          `${this.key.location}：`,
-          `${this.key.targetClasses}：`,
-          `${this.key.deadline}：`,
-          `${this.key.category}：`,
-        ].join('\n')
-      );
-      this.line.pushText(
-        to,
-        [
-          '↑埋めて返信してください↑',
-          '※記載上の注意',
-          `${this.keyDescription.date}`,
-          `${this.keyDescription.time}`,
-          `${this.keyDescription.title}`,
-          `${this.keyDescription.location}`,
-          `${this.keyDescription.targetClasses}`,
-          `${this.keyDescription.deadline}`,
-          categoryText,
-        ].join('\n\n')
-      );
-      return;
-    }
-
-    if (text.trim().includes('外部練追加フォーマット')) {
-      // this.line.pushText(to, '外部練追加作業を行います。少々お待ちください。');
-
-      text = StringUtils.toHalfWidth(text);
-      const exPracrtice = this.parseExternalPractice(text);
-      if (!exPracrtice) {
-        this.line.pushText(to, 'フォーマットが正しくありません。再度ご確認ください。');
-        return;
-      }
-
-      const calendarDescription = this.configOfExPracrice.getByName(
-        exPracrtice.category
-      )?.description;
-
-      try {
-        this.calendar.createCalenderEvent(
-          {
-            start: exPracrtice.date,
-            location: exPracrtice.location,
-            summary: `外部${exPracrtice.title}${exPracrtice.timeRange} ${
-              exPracrtice.targetClasses
-            }:${DateUtils.formatMD(exPracrtice.deadline)}〆`,
-            description: calendarDescription,
-          },
-          Config.Calendar.id.externalPractice
-        );
-      } catch (e) {
-        this.line.pushError((e as Error).message);
-      }
-      try {
-        this.calendar.createCalenderEvent(
-          {
-            start: exPracrtice.deadline,
-            summary: `〆${exPracrtice.targetClasses}|外部${exPracrtice.title}${DateUtils.formatMDD(
-              exPracrtice.date
-            )}`,
-            description: `グループLINE内「イベント」から参加ポチ\nKM練は調整さん入力も忘れずに！`,
-          },
-          Config.Calendar.id.internalDeadline
-        );
-      } catch (e) {
-        this.line.pushError((e as Error).message);
-      }
-
-      this.line.pushText(
-        to,
-        [
-          `✅ 外部練習・締切日を登録しました`,
-          `日付: ${DateUtils.formatMDD(exPracrtice.date)}`,
-          `時間: ${exPracrtice.timeRange}`,
-          `練習名: ${exPracrtice.title}`,
-          `対象級: ${exPracrtice.targetClasses}`,
-          `〆切: ${DateUtils.formatMDD(exPracrtice.deadline)}`,
-          `場所: ${exPracrtice.location}`,
-          `種別: ${exPracrtice.category}`,
-        ].join('\n')
-      );
-      return;
-    }
+  // 外部練追加フォーマットの返信
+  private replyFormat(to: string) {
+    const categoryList = [...this.configOfExPracrice.names(), 'その他'];
+    const categoryText = `種別:\n下記のどれかを選んでください。\n${categoryList.join('/')}`;
+    this.line.pushText(
+      to,
+      [
+        '★外部練追加フォーマット★',
+        `${this.key.date}：`,
+        `${this.key.time}：`,
+        `${this.key.title}：`,
+        `${this.key.location}：`,
+        `${this.key.targetClasses}：`,
+        `${this.key.deadline}：`,
+        `${this.key.category}：`,
+      ].join('\n')
+    );
+    this.line.pushText(
+      to,
+      [
+        '↑埋めて返信してください↑',
+        '※記載上の注意',
+        `${this.keyDescription.date}`,
+        `${this.keyDescription.time}`,
+        `${this.keyDescription.title}`,
+        `${this.keyDescription.location}`,
+        `${this.keyDescription.targetClasses}`,
+        `${this.keyDescription.deadline}`,
+        categoryText,
+      ].join('\n\n')
+    );
+    return;
   }
 
+  // 外部練追加の処理
+  private registerExternalPractice(text: string, to: string) {
+    text = StringUtils.toHalfWidth(text);
+    const exPracrtice = this.parseExternalPractice(text);
+    if (!exPracrtice) {
+      this.line.pushText(to, 'フォーマットが正しくありません。再度ご確認ください。');
+      return;
+    }
+
+    const calendarDescription = this.configOfExPracrice.getByName(
+      exPracrtice.category
+    )?.description;
+
+    try {
+      this.calendar.createCalenderEvent(
+        {
+          start: exPracrtice.date,
+          location: exPracrtice.location,
+          summary: `外部${exPracrtice.title}${exPracrtice.timeRange} ${
+            exPracrtice.targetClasses
+          }:${DateUtils.formatMD(exPracrtice.deadline)}〆`,
+          description: calendarDescription,
+        },
+        Config.Calendar.id.externalPractice
+      );
+    } catch (e) {
+      this.line.pushError((e as Error).message);
+    }
+    try {
+      this.calendar.createCalenderEvent(
+        {
+          start: exPracrtice.deadline,
+          summary: `〆${exPracrtice.targetClasses}|外部${exPracrtice.title}${DateUtils.formatMDD(
+            exPracrtice.date
+          )}`,
+          description: `グループLINE内「イベント」から参加ポチ\nKM練は調整さん入力も忘れずに！`,
+        },
+        Config.Calendar.id.internalDeadline
+      );
+    } catch (e) {
+      this.line.pushError((e as Error).message);
+    }
+
+    this.line.pushText(
+      to,
+      [
+        `✅ 外部練習・締切日を登録しました`,
+        `日付: ${DateUtils.formatMDD(exPracrtice.date)}`,
+        `時間: ${exPracrtice.timeRange}`,
+        `練習名: ${exPracrtice.title}`,
+        `対象級: ${exPracrtice.targetClasses}`,
+        `〆切: ${DateUtils.formatMDD(exPracrtice.deadline)}`,
+        `場所: ${exPracrtice.location}`,
+        `種別: ${exPracrtice.category}`,
+      ].join('\n')
+    );
+    return;
+  }
+
+  // 外部練追加フォーマットのパース
   private parseExternalPractice(text: string): ExPracticeEvent | null {
     // 行ごとに key：value を抽出
     const lines = text
